2daa45fa992c4a7b7746979e0d202d9cd1b04cc9
Oleksandr Yanov <oleksandr.yanov@gmail.com>
2022-02-07 09:31:23 +0200

    added dev specific methods

--- portal_api/users_api.py
+++ portal_api/users_api.py
@@ -1,2 +1,5 @@
 def add_user(name):
-    print("Adding user:", name)
+    print("DEV, Adding user:", name)
+
+def dev_specific_delete_user(name):
+    print("dev_specific_delete_user, name:", name)
--- portal_page_objects/login_page.py
+++ portal_page_objects/login_page.py
@@ -1,2 +1,5 @@
 def login(name):
-    print("Log in, name:", name)
+    print("DEV, Log in, name:", name)
+
+def dev_specific_logout(name):
+    print("dev_specific_logout, name:", name)
