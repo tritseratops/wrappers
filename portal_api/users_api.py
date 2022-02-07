cbcc83cde98595e1b461524233eba76c7a902eb0
Oleksandr Yanov <oleksandr.yanov@gmail.com>
2022-02-07 09:28:16 +0200

    added new branch and specific methods

--- portal_api/users_api.py
+++ portal_api/users_api.py
@@ -1,2 +1,5 @@
 def add_user(name):
-    print("Adding user:", name)
+    print("Staging: Adding user:", name)
+
+def specific_stagin_delete_user(name):
+    print("specific_stagin_delete_user:", name)
--- portal_page_objects/login_page.py
+++ portal_page_objects/login_page.py
@@ -1,2 +1,5 @@
 def login(name):
     print("Log in, name:", name)
+
+def specific_stagin_logout_user(name):
+    print("specific_stagin_logout_user:", name)
